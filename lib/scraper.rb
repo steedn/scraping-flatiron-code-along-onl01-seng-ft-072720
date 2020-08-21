d1079e34e6a94a650f3cf5af032996d265aaf00d
Nathan Steed <steedn@outlook.com>
2020-08-21 00:01:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,6 +13,10 @@ class Scraper
     #  course.description = post.css("p").text
   #  end
   end
+  def get_courses
+  self.get_page.css(".post")
+end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
